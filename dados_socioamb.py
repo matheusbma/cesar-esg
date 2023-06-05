5923691a5b09052847f194b8d0a52a78c9bc5da0
João Bosco <junior232.jb@gmail.com>
2023-06-05 10:55:10 -0300

    Tabelas

---
+++ dados_socioamb.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Indicador": [
+            "Consumo mensal de Energia Elétrica",
+            "Proporção de Energia elétrica consumida do Mercado Livre",
+            "Proporção de Energia elétrica consumida do Mercado Cativo",
+            "Peso total de resíduos sólidos produzidos",
+            "",
+            "",
+            "Gravimetrida dos resíduos sólidos",
+            "",
+            "",
+            "",
+            "",
+            "Proporção de Resíduos destinados ao aterro",
+            "Proporção de resíduos destindos à reciclagem",
+            "Consumo Mensal de Água",
+            "Proporção da água tratada que é consumida",
+            "Consumo mensal de combustível",
+            "Qualidade do ar no ambiente de trabalho",
+            "Resíduos Eletrônicos",
+            "Pilhas"
+        ],
+
+        "Unidade": [
+            "115 KW",
+            "46,7%",
+            "53,3%",
+            "3.950Kg",
+            "Orgânicos e rejeitos: 1.263 kg",
+            "Papel/Papelão: 1.841kg",
+            "Plástico: 257kg",
+            "Metais: 72 kg",
+            "Eletrônicos 505kg",
+            "Pilhas e baterias 12kg",
+            "32%",
+            "68%",
+            "44m³",
+            "100%",
+            "442,9L",
+
+
+
+            
+        ],
+
+        "Escopo": [
+            "janeiro a setembro de 2022",
+            "janeiro a setembro de 2022",
+            "janeiro a setembro de 2022",
+            "janeiro a setembro de 2022",
+            "",
+            "",
+            "janeiro a setembro de 2022"
+            "",
+            "",
+            "",
+            "306-5 Waste directed to disposal",
+            "306-4 Waste diverted from disposal",
+            "303-5a Water consumption",
+            "",
+            "302-1 Energy consumption within the organization",
+            "",
+            "",
+            "306-3 Waste generated",
+        ],
+
+        "GRI Standard Disclosure": [
+
+        ],
+
+        "ODS": [
+            "12, 2",
+            "12, 2",
+            "12, 2",
+            "12, 5",
+            "",
+            "",
+            "12, 5",
+            "",
+            "",
+            "",
+            "12, 5",
+            "12, 5",
+            "12, 2",
+            "12, 2",
+            "12, 2",
+            "12, 4",
+            "12, 4",
+            "12, 5",
+        ]
+    }
+)
+
